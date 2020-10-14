23c3faa534e7d4a7a072e81179b52b95350f4949
PierreLepagnol <pierre@lepagnol.net>
2020-10-14 10:48:05 +0200

    Pierre Update ManipData.py + PierreLep.py

--- PierreLep.py
+++ PierreLep.py
@@ -6,4 +6,55 @@ Created on Mon Oct 12 21:59:13 2020
 @author: PierreLepagnol
 """
 
-Import(fileName='unknown',fileExtension='csv')
+from sklearn import model_selection
+
+from sklearn import feature_extraction
+from sklearn import linear_model
+from sklearn import pipeline
+from sklearn import preprocessing
+from sklearn import metrics
+from manipData import Import,Export,cleanText,deleteStopWords
+import pandas as pd
+
+
+# Nom des jobs
+names = pd.read_csv('./data/categories_string.csv')['0'].to_dict()
+
+data=Import(fileName='./data/train',fileExtension='json').set_index('Id')
+train_label=pd.read_csv('./data/train_label.csv',index_col='Id')
+
+
+X_train=data["description"].apply(deleteStopWords)
+Y_train=train_label
+
+DF= X_train.map(names)
+
+model = pipeline.make_pipeline(
+    feature_extraction.text.TfidfVectorizer(),
+    preprocessing.Normalizer(),
+    linear_model.LogisticRegression(multi_class='multinomial')
+)
+
+model = model.fit(X_train, Y_train)
+
+y_pred = model.predict(X_test)
+# y_pred = pd.Series(y_pred, name='job', index=X_test.index)
+
+# print(metrics.f1_score(y_test, y_pred, average='macro'))
+
+# # dtype={'Id':'float','description':'string','gender':'category'}
+# # Train_X=Import(fileName='./data/train',fileExtension='json',dtype=dtype)
+# # # Train_Label=Import(fileName='./data/train_label',fileExtension='csv')
+
+# # # Test_X=Import(fileName='./data/test',fileExtension='json')
+
+
+# # # Train_X = Train_X.set_index('Id')
+# # res=Train_X.apply(lambda x : cleanText(x['description']),axis=1)
+
+
+
+
+# test_people = pd.concat((y_pred, gender_test), axis='columns')
+
+# macro_disparate_impact(test_people)
--- manipData.py
+++ manipData.py
@@ -9,7 +9,9 @@ Created on Mon Oct 12 10:18:10 2020
 """
 
 import os
+import re
 import pandas as pd
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 def KaggleSubmit(fileName,Msg):
     """  Soumission sur kaggle\n
@@ -30,18 +32,57 @@ def Import(fileName='unknown',fileExtension='csv'):
     Params:
     fileName (str) Nom du fichier\t
     fileExtension (str) Extension du fichier
-    
+
     Returns: dataset: DataFrame Pandas
     """
     fileName_Ext=f"{fileName}.{fileExtension}"
-    cmdread = f'pd.read_{fileExtension}(f)'
-    with open(fileName_Ext,'r') as f:
-        dataset=eval(cmdread) 
-    print(cmdread)
+    cmdread = f"pd.read_{fileExtension}('{fileName_Ext}')"
+    dataset=eval(cmdread)  
     return dataset
 
-def Export(pdDF=pd.DataFrame(),fileName='./data/unknown',fileExtension='csv',KaggleSubmission=False,MsgSubmit='New Submission from DataBrothers'):
+def Export(pdDF=pd.DataFrame(),fileName='./data/unknown',fileExtension='csv',
+           KaggleSubmission=False,MsgSubmit='New Submission from DataBrothers'):
     fileName_Ext=f"{fileName}.{fileExtension}"
-    df.to_csv(fileName_Ext, index = False, header=True)
+    
+    cmdto = f"pdDF.to_{fileExtension}('{fileName_Ext}', index = False, Header=True)"
+    eval(cmdto)
     if(KaggleSubmission): KaggleSubmit(fileName_Ext,MsgSubmit)
     return 0
+
+def deleteStopWords(desc):
+    return ' '.join([word for word in desc.split() if word not in ENGLISH_STOP_WORDS])
+
+def cleanText(string: str, punctuations=r'''!()-[]{};:'"\,<>./?@#$%^&*_~''', 
+               stop_words=['the', 'a', 'and', 'is', 'be', 'will','on'])->str:
+    """
+    A method to clean text 
+    """
+    # Cleaning the urls
+    string = re.sub(r'https?://\S+|www\.\S+', '', string)
+
+    # Cleaning the html elements
+    string = re.sub(r'<.*?>', '', string)
+
+    # Removing the punctuations
+    for x in string.lower(): 
+        if x in punctuations: 
+            string = string.replace(x, "") 
+
+    # Converting the text to lower
+    string = string.lower()
+
+    # Removing stop words
+    string = ' '.join([word for word in string.split() if word not in stop_words])
+
+    # Cleaning the whitespaces
+    string = re.sub(r'\s+', ' ', string).strip()
+
+    return string  
+
+def macro_disparate_impact(people):
+    counts = people.groupby(['job', 'gender']).size().unstack('gender')
+    counts['disparate_impact'] = counts[['M', 'F']].max(axis='columns') / counts[['M', 'F']].min(axis='columns')
+    return counts['disparate_impact'].mean()
+
+def macro_f1(y_true, y_pred):
+    return f1_score(y_true, y_pred, average='macro')
